ec967c7adf921084911dc4b4d5a7f99b06bb1503
Rikikangsc2 <112865967+Rikikangsc2@users.noreply.github.com>
2024-07-27 06:07:11 +0000

    H

--- app.js
+++ app.js
@@ -1,5 +1,30 @@
 const express = require('express');
 const app = express();
+const tesseract = require("node-tesseract-ocr")
+
+const config = {
+  lang: "ind",
+  oem: 1,
+  psm: 3,
+}
+
+app.get('/ocr', async (req, res) =>{
+  if (req.query.url) return res.status(400).json({ error: "url is required" })
+  const img = 
+
+tesseract
+  .recognize(img, config)
+  .then((text) => {
+    res.status(200).json({
+      text: text
+    })
+  })
+  .catch((error) => {
+    res.status(500).json({
+      error: error
+    })
+  })
+});
 
 app.get('/', (req, res) => {
   res.send('Hallo World!');
