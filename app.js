a57d843ab1a6960300acf424a0d0be4bdf8fb15f
Rikikangsc2 <112865967+Rikikangsc2@users.noreply.github.com>
2024-10-14 02:12:02 +0000

    Hh

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+//++++
+const { ttdl } = require('btch-downloader')
+
+app.get('/ttdl', async (req, res) => {
+  try {
+    const url = req.query.url;
+    const data = await ttdl(url);
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error downloading video');
+  }
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
