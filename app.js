d0415e7c894ccfb41601701421f237d4cc244873
Rikikangsc2 <112865967+Rikikangsc2@users.noreply.github.com>
2024-10-15 06:11:07 +0000

    Anjay

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 3000;
 // Set EJS as templating engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.set('json spaces', 2);
 
 // Static files
 app.use(express.static(path.join(__dirname, 'public')));
--- routes/api.js
+++ routes/api.js
@@ -4,8 +4,55 @@ const axios = require('axios');
 const fs = require('fs');
 const path = require('path');
 const { handleChat } = require('../module/llama');
+const {twitter, igdl, ttdl,fbdown} = require('btch-downloader');
 
 ///----
+router.get('/twitter', async (req, res) => {
+  const url = req.query.url;
+  if (!url) return res.json({ error: 'Missing url parameter' });
+  try {
+    const data = await twitter(url);
+    res.json(data);
+  } catch (err) {
+    console.error(err);
+    res.json({ error: 'Failed to download' });
+  }
+});
+router.get('/fbdown', async (req, res) => {
+  const url = req.query.url;
+  if (!url) return res.json({ error: 'Missing url parameter' });
+  try {
+    const data = await fbdown(url);
+    res.json(data);
+  } catch (err) {
+    console.error(err);
+    res.json({ error: 'Failed to download' });
+  }
+});
+router.get('/ttdl', async (req, res) => {
+  const url = req.query.url;
+  if (!url) return res.json({ error: 'Missing url parameter' });
+  try {
+    const data = await ttdl(url);
+    data.creator = "PurAPI"
+    res.json(data);
+  } catch (err) {
+    console.error(err);
+    res.json({ error: 'Failed to download' });
+  }
+});
+router.get('/igdl', async (req, res) => {
+  const url = req.query.url;
+  if (!url) return res.json({ error: 'Missing url parameter' });
+  try {
+    const data = await igdl(url);
+    data[0].wm = "PurAPI"
+    res.json(data);
+  } catch (err) {
+    console.error(err);
+    res.json({ error: 'Failed to download' });
+  }
+});
 router.get('/llama',async(req,res)=>{
   if (!req.query.user&&!req.query.text&&!req.query.systemPrompt) return res.json({error: "Missing query parameter"});
   handleChat(req,res,null);
