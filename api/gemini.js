e8415cb45887df80afe86cedb81fd537882a20b3
Rikikangsc2 <112865967+Rikikangsc2@users.noreply.github.com>
2024-11-08 02:41:43 +0000

    Anj

--- api/gemini.js
+++ api/gemini.js
@@ -7,11 +7,11 @@ const apiKeys = [
   'AIzaSyAS6CLgV1nFuSksdMBwo4gQfro1fHUFBHU'
 ];
 
-// Get a random API key
 const getRandomApiKey = () => {
   const randomIndex = Math.floor(Math.random() * apiKeys.length);
   return apiKeys[randomIndex];
 };
+
 const apiKey = getRandomApiKey();
 const genAI = new GoogleGenerativeAI(apiKey);
 
@@ -19,20 +19,18 @@ const generationConfig = {
   temperature: 1,
   topP: 0.95,
   topK: 40,
-  maxOutputTokens: 8192,
+  maxOutputTokens: 200,
   responseMimeType: "text/plain",
 };
 
 const dbPath = 'db/data.json';
 
-// Ensure db/data.json exists and is initialized as an empty object if not
 const initializeDb = () => {
   if (!fs.existsSync(dbPath)) {
     fs.writeFileSync(dbPath, JSON.stringify({}), 'utf8');
   }
 };
 
-// Load chat history for a specific user
 const loadHistory = (user) => {
   try {
     initializeDb();
@@ -44,12 +42,11 @@ const loadHistory = (user) => {
   }
 };
 
-// Save chat history for a specific user
 const saveHistory = (user, history) => {
   try {
     initializeDb();
     const data = JSON.parse(fs.readFileSync(dbPath, 'utf8'));
-    if (history.length > 30) history.shift();  // Limit history to 30 entries
+    if (history.length > 30) history.shift();
     data[user] = history;
     fs.writeFileSync(dbPath, JSON.stringify(data, null, 2), 'utf8');
   } catch (error) {
@@ -57,13 +54,11 @@ const saveHistory = (user, history) => {
   }
 };
 
-// Main function to handle requests
 module.exports = async (req, res) => {
   const { text, systemPrompt, user } = req.query;
   let history = loadHistory(user);
 
   try {
-    // Create a new chat session with the provided systemPrompt for systemInstruction
     const model = genAI.getGenerativeModel({
       model: "gemini-1.5-flash",
       systemInstruction: systemPrompt || "Kamu adalah Alicia AI yang ramah, dan akrab!.",
@@ -77,13 +72,14 @@ module.exports = async (req, res) => {
     const result = await chatSession.sendMessage(text);
     const responseText = result.response.text();
 
-    // Update history with user and model messages
     history.push({ role: "user", parts: [{ text }] }, { role: "model", parts: [{ text: responseText }] });
     saveHistory(user, history);
 
     res.json({ result: responseText });
   } catch (error) {
     console.error('Error during chat session:', error);
+    history.splice(-5, 5);
+    saveHistory(user, history);
     res.status(500).json({ error: 'An error occurred while processing the request.' });
   }
 };
