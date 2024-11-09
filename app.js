71953a5781a3f46966f9f92d6f6cdba93a83a3b9
Rikikangsc2 <112865967+Rikikangsc2@users.noreply.github.com>
2024-11-09 00:15:01 +0000

    Add Db for chat

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const aio = require('./api/aio');
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.set('json spaces', 2);
-
+app.use(express.static(path.join(__dirname, 'db')));
 app.get('/yt',yt)
 app.get('/gemini',gemini)
 app.get('/aio', aio)
